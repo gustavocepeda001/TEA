b273d586aa8a4e315af5b6c6fcf76eccbca82bb8
Gustavo Sebastian Cepeda Solis (24179) <gustavo.cepeda@est.zamorano.edu>
2022-09-30 15:06:31 -0600

    https://github.com/gustavocepeda001/TEA.git

---
+++ lab05/ejercicio1.py
@@ -0,0 +1,10 @@
+from turtle import clear
+
+
+texto = "X-DSPAM-Confidence:0.8475"
+inicio = texto.find(":") + 1 
+final =len(texto)
+numero = float(texto[inicio:final])
+print(inicio, final)
+print(numero)
+print(type(numero))
